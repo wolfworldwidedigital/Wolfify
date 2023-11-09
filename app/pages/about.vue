128aac729c13f8fc4d921af4e5081a53ed40dce5
David De Wolf <admin@wolfworldwide.digital>
2023-11-09 14:52:47 +0100

    Builder.io set up

---
+++ app/pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <RenderContent model="wolfify" />
+  </template>
+  <script>
+    import { RenderContent, builder } from '@builder.io/vue'
+  
+    builder.init("27cace4a848242e1979ab3df0afb6dae")
+  
+    export default { 
+      components: { RenderContent }
+    }
+  </script>
